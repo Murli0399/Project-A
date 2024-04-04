9a79b432773575dfeda143a2d2c05fd368cd62ac
Murli0399 <murlikhaire28@gmail.com>
2024-04-04 19:13:02 +0530

    added

--- demo/src/main/java/com/murli/security/controller/AuthController.java
+++ demo/src/main/java/com/murli/security/controller/AuthController.java
@@ -1,9 +1,7 @@
 package com.murli.security.controller;
 
-
 import com.murli.security.dto.JwtResponse;
 import com.murli.security.dto.LoginRequest;
-import com.murli.security.dto.UserInfo;
 import com.murli.security.service.JwtHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,16 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
-
 
 @RestController
 @RequestMapping("/auth")
@@ -72,5 +66,4 @@ public class AuthController {
 
 	}
 
-	
 }
